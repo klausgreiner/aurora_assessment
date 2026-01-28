9df7728ecdbe6e2821409d616a4c6b93008aa60d
Klaus Greiner <klausgreinerfiorini@gmail.com>
2026-01-28 11:47:28 +0100

    Set app orientation to portrait mode for both Android and iOS platforms, ensuring a consistent user experience across devices.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'core/core.dart';
@@ -6,6 +7,11 @@ import 'core/core.dart';
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   setupDI();
 
